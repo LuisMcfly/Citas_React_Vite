ad11861d02bf54a8bf2d4052245a07fe42795c7e
LuisMcfly <luis.payares.ruiz@gmail.com>
2022-10-31 06:17:22 -0500

    UI

---
+++ src/components/Paciente.jsx
@@ -0,0 +1,13 @@
+const Paciente = () => {
+  return (
+    <div className="m-3 bg-white shadow-md rounded-lg py-10 px-5">
+          <p className="font-bold mb-3 text-gray-700 uppercase">Nombre:<span className="font-normal normal-case"> Gordo</span></p>
+          <p className="font-bold mb-3 text-gray-700 uppercase">Propietario:<span className="font-normal normal-case"> Luis</span></p>
+          <p className="font-bold mb-3 text-gray-700 uppercase">Email:<span className="font-normal normal-case"> Admin@admin.com</span></p>
+          <p className="font-bold mb-3 text-gray-700 uppercase">Alta:<span className="font-normal normal-case"> 31 Octubre 2022</span></p>
+          <p className="font-bold mb-3 text-gray-700 uppercase">Síntomas:<span className="font-normal normal-case"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam doloremque cumque assumenda earum ab esse corrupti. Quisquam officia itaque, vitae blanditiis consectetur nesciunt incidunt est eligendi tempora neque odio numquam.</span></p>
+        </div>
+  )
+}
+
+export default Paciente
