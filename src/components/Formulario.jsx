641ada11015b6e846a4423bf2492e6821a2f2b7e
LuisMcfly <luis.payares.ruiz@gmail.com>
2022-11-05 03:26:26 -0500

    Validacion de formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -7,10 +7,19 @@ function Formulario() {
   const [alta, setAlta] = useState('');
   const [sintomas, setSintomas] = useState('');
 
+  const [error, setError] = useState(false);
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    console.log('Se esta enviando el formulario');
+    // Validancion del formulario
+    if([ nombre,propietario,email,alta,sintomas ].includes('') ) {
+      setError(true);
+      return;
+    }
+    
+    setError(false);
+
   }
 
   return (
@@ -19,6 +28,11 @@ function Formulario() {
         <p className='text-lg mt-5 text-center mb-10'>Añade Pacientes y<span className='text-indigo-600 font-bold'> Administralos</span></p>
 
         <form onSubmit={handleSubmit} className="bg-white shadow-md rounded-lg py-10 px-5">
+          { error && (
+                <div className='bg-red-800 text-white text-center p-3 uppercase font-bold mb-3 rounded'>
+                  <p>Todos los campos son obligatorios</p>
+                </div>
+          )}
           <div className="mb-5">
             <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold">Marcota {nombre}</label>
             <input id="mascota" type="text" placeholder="Nombre de la mascota" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" value={nombre} 
