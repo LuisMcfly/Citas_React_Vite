6c9d7772ff08ad13e5075c972035c957f743aa4e
LuisMcfly <luis.payares.ruiz@gmail.com>
2022-11-06 06:58:19 -0500

    Eliminar paciente

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,6 +1,13 @@
-const Paciente = ({paciente, setPaciente}) => {
+const Paciente = ({paciente, setPaciente, eliminarPaciente}) => {
 
-  const { nombre, propietario, email, alta, sintomas} = paciente
+  const { nombre, propietario, email, alta, sintomas, id} = paciente
+
+  const handleEliminar = () => {
+    const respuesta = confirm('¿Deseas eliminar este paciente?');
+    if(respuesta){
+      eliminarPaciente(id)
+    }
+  }
 
   return (
     <div className="mx-5 my-10 bg-white shadow-md rounded-lg py-10 px-5">
@@ -11,7 +18,7 @@ const Paciente = ({paciente, setPaciente}) => {
           <p className="font-bold mb-3 text-gray-700 uppercase">Síntomas: <span className="font-normal normal-case">{sintomas}</span></p>
           <div className="flex flex-col gap-4 mt-10">
             <button type="button" className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white uppercase font-bold rounded-lg" onClick={() => setPaciente(paciente)}>editar</button>
-            <button type="button" className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white uppercase font-bold rounded-lg">eliminar</button>
+            <button type="button" className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white uppercase font-bold rounded-lg" onClick={handleEliminar}>eliminar</button>
           </div>
         </div>
   )
