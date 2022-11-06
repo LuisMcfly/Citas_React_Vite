9d8b733f1f143387e68bc59fb4a5c2966c8a228d
LuisMcfly <luis.payares.ruiz@gmail.com>
2022-11-06 07:25:54 -0500

    LocalStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Formulario from "./components/Formulario";
 import Header from "./components/Header";
 import ListadoPacientes from "./components/ListadoPacientes";
@@ -7,6 +7,19 @@ function App() {
 
   const [pacientes, setPacientes] = useState([]);
   const [paciente, setPaciente] = useState({});
+
+  useEffect(() => {
+    const obtenerLs = () => {
+      const pacientesLs = JSON.parse(localStorage.getItem('pacientes')) ?? [];
+      setPacientes(pacientesLs)
+    }
+
+    obtenerLs();
+  }, []) 
+
+  useEffect(() => {
+    localStorage.setItem('pacientes', JSON.stringify(pacientes))
+  }, [pacientes])
 
   const eliminarPaciente = id => {
     const pacientesActualizados = pacientes.filter( paciente => paciente.id !== id);
